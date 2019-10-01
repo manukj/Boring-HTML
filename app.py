86137f573f8c40b406684655a3c26d9b062c75e8
manukj <manu1998kj@gmail.com>
2019-10-01 06:25:16 +0530

    validation using firebase v1

--- app.py
+++ app.py
@@ -10,18 +10,23 @@ from subprocess import call
 
 app = Flask(__name__,template_folder='template')
 
+login_status = 0
 
-config = {
-  "apiKey": "AIzaSyA7UUJVAOEjWSaP6uezJRx-OJPLX-n8-kU",
-"authDomain": "boringhtml-a6697.firebaseapp.com",
-"databaseURL": "https://boringhtml-a6697.firebaseio.com",
-"projectId": "boringhtml-a6697",
-"storageBucket": "boringhtml-a6697.appspot.com",
-"serviceAccount": "firebase.json"
-}
+firebaseConfig = {
+  'apiKey': "AIzaSyD8N-HKhsTOmo-4IgxbTXmlVZb1ZmWpVg0",
+  'authDomain': "boring-html.firebaseapp.com",
+  'databaseURL': "https://boring-html.firebaseio.com/",
+  'projectId': "boring-html",
+  'storageBucket': "boring-html.appspot.com",
+  'messagingSenderId': "772540172270",
+  'appId': "1:772540172270:web:aa588f06a3e9779adef0ff",
+  'measurementId': "G-1T98ECTMSF"
+};
 
-firebase = pyrebase.initialize_app(config)
-firebase = FirebaseApplication('https://boringhtml-a6697.firebaseio.com', None)
+firebase = pyrebase.initialize_app(firebaseConfig)
+
+auth = firebase.auth()
+
 
 
 @app.route("/")
@@ -33,25 +38,39 @@ def renderLoginAccessPage():
     if request.method == 'POST':
         data = request.form.to_dict()
         print("JSON data: ", data)
-        print(type(data.get("username")))
-        result = firebase.get('/'+str(data.get("username")), None)
-        if result!= None:
-            hashed = result.get("password").encode('utf-8')
-            password = data.get("password").encode('utf-8')
-            print('hashed',hashed)
-            print('password',password)
-            # print('result.get("pswd")',type(result.get("password")))
-            # print('str(data.get("password"))',type(data.get("password")))
-            if bcrypt.checkpw(password, hashed):
-                print("successful")
-                # return render_template("dashboard.html")
-                call(["php","dashboard.php"])
-            else:
-                print("unsuccessful")
-                return render_template("login.html" , status = "fail" )
-        else:
-            print("unsuccessful")
-            return render_template("login.html" , status = "fail" )
+        email = str(data.get("userName"))
+        password = str(data.get("password"))
+        try:
+            user = auth.sign_in_with_email_and_password(email,password)
+            print("added")
+            render_template('dashboard.html',result = data)
+        except:
+            print("uncessful")
+            render_template("login.html",us = "fail")
+
+    # if request.method == 'POST':
+    #     data = request.form.to_dict()
+    #     print("JSON data: ", data)
+    #     print(type(data.get("username")))
+    #     result = firebase.get('/'+str(data.get("username")), None)
+    #     if result!= None:
+    #         hashed = result.get("password").encode('utf-8')
+    #         password = data.get("password").encode('utf-8')
+    #         print('hashed',hashed)
+    #         print('password',password)
+    #         print(result)
+    #         # print('result.get("pswd")',type(result.get("password")))
+    #         # print('str(data.get("password"))',type(data.get("password")))
+    #         if bcrypt.checkpw(password, hashed):
+    #             print("successful")
+    #             # return render_template("dashboard.html")
+    #             return render_template("dashboard.html",result = result)
+    #         else:
+    #             print("unsuccessful")
+    #             return render_template("login.html" , status = "fail" )
+        # else:
+        #     print("unsuccessful")
+        #     return render_template("login.html" , status = "fail" )
 
     return render_template("login.html")
 
@@ -61,41 +80,53 @@ def registerUser():
     error = None
     if request.method == 'POST':
         data = request.form.to_dict()
-        print("data = " ,data)
-        username = str(data.get("userName"))
+        print(data)
+        # username = str(data.get("userName"))
+        email = str(data.get("userName"))
         firstName = str(data.get("firstName"))
         lastName = str(data.get("lastName"))
         password = str(data.get("password"))
-        password = password.encode('ASCII')
-        hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+        # password = password.encode('ASCII')
+        # hashed = bcrypt.hashpw(password, bcrypt.gensalt())
         data = {}
         data["firstName"] = firstName
         data["lastName"] = lastName
-        data["password"] = hashed
-        
-        firebase = pyrebase.initialize_app(config)
-        firebase = FirebaseApplication('https://boringhtml-a6697.firebaseio.com', None)
-        result = firebase.get('/'+username, None)
-        print ("result = ",result)
-        if result== None:
-            print("adding new user")
-            firebase = pyrebase.initialize_app(config)
-            db = firebase.database()
-            db.child(username).set(data)
-            print("added")
-            return render_template("dashboard.html")
-        else:
-            error = "User already exists"
-            print(error)
-            return render_template("register.html")
-    return render_template("register.html")
+        data["password"] = password
+        data["project"] = 0
+        # firebase = FirebaseApplication('https://boring-html.firebaseio.com/', None)
+        firebase = pyrebase.initialize_app(firebaseConfig)
+
+        auth = firebase.auth()
+        try:
+            auth.create_user_with_email_and_password(email,password)
+        except Exception as e:
+            return render_template("register.html",us = e)
+        db = firebase.database()
+        db.child(email).set(data)
+        print("added")
+        return render_template('dashboard.html',result = data)
+
+        #result = firebase.get('/'+username, None)
+    
+        # if result== None:
+        #     firebase = pyrebase.initialize_app(config)
+        #     db = firebase.database()
+        #     db.child(username).set(data)
+        #     print("added")
+        #     render_template('dashboard.html',result = result)
+        # else:
+        #     error = "User already exists"
+        #     print(error)
+        #     return render_template("register.html")
+    else:
+        return render_template("register.html")
 
 
-
-# dashboard rendering 
-@app.route("/renderDashboard")
+# logout
+@app.route("/login")
 def dashboard():
-    return render_template("dashboard.html")
+    login_status = 0;
+    return render_template('index.html')
 
 if __name__ == '__main__':
    flag_login = 0
