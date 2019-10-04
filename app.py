21d11b79a1ed9ad226e5abe6fc896fcf4ca09b3b
manukj <manu1998kj@gmail.com>
2019-10-04 23:18:36 +0530

    firebase for register bug fixed

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ login_status = 0
 firebaseConfig = {
   'apiKey': "AIzaSyD8N-HKhsTOmo-4IgxbTXmlVZb1ZmWpVg0",
   'authDomain': "boring-html.firebaseapp.com",
-  'databaseURL': "https://boring-html.firebaseio.com/",
+  'databaseURL': "https://boring-html.firebaseio.com",
   'projectId': "boring-html",
   'storageBucket': "boring-html.appspot.com",
   'messagingSenderId': "772540172270",
   'appId': "1:772540172270:web:aa588f06a3e9779adef0ff",
   'measurementId': "G-1T98ECTMSF"
-};
+}
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 
@@ -44,34 +44,9 @@ def renderLoginAccessPage():
             user = auth.sign_in_with_email_and_password(email,password)
             print("added")
             render_template('dashboard.html',result = data)
-        except:
-            print("uncessful")
-            render_template("login.html",us = "fail")
-
-    # if request.method == 'POST':
-    #     data = request.form.to_dict()
-    #     print("JSON data: ", data)
-    #     print(type(data.get("username")))
-    #     result = firebase.get('/'+str(data.get("username")), None)
-    #     if result!= None:
-    #         hashed = result.get("password").encode('utf-8')
-    #         password = data.get("password").encode('utf-8')
-    #         print('hashed',hashed)
-    #         print('password',password)
-    #         print(result)
-    #         # print('result.get("pswd")',type(result.get("password")))
-    #         # print('str(data.get("password"))',type(data.get("password")))
-    #         if bcrypt.checkpw(password, hashed):
-    #             print("successful")
-    #             # return render_template("dashboard.html")
-    #             return render_template("dashboard.html",result = result)
-    #         else:
-    #             print("unsuccessful")
-    #             return render_template("login.html" , status = "fail" )
-        # else:
-        #     print("unsuccessful")
-        #     return render_template("login.html" , status = "fail" )
-
+        except Exception as e:
+            print(e)
+            render_template("login.html",re = "jhh")
     return render_template("login.html")
 
 
@@ -92,6 +67,7 @@ def registerUser():
         data["firstName"] = firstName
         data["lastName"] = lastName
         data["password"] = password
+        data["email"] = email
         data["project"] = 0
         # firebase = FirebaseApplication('https://boring-html.firebaseio.com/', None)
         firebase = pyrebase.initialize_app(firebaseConfig)
@@ -102,22 +78,9 @@ def registerUser():
         except Exception as e:
             return render_template("register.html",us = e)
         db = firebase.database()
-        db.child(email).set(data)
+        db.child(firstName).set(data)
         print("added")
         return render_template('dashboard.html',result = data)
-
-        #result = firebase.get('/'+username, None)
-    
-        # if result== None:
-        #     firebase = pyrebase.initialize_app(config)
-        #     db = firebase.database()
-        #     db.child(username).set(data)
-        #     print("added")
-        #     render_template('dashboard.html',result = result)
-        # else:
-        #     error = "User already exists"
-        #     print(error)
-        #     return render_template("register.html")
     else:
         return render_template("register.html")
 
