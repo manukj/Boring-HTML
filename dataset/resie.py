075ea5e139e0c572f509233cf13bcddc5be88128
krs <rsri1998@gmail.com>
2019-10-24 21:13:16 +0530

    dataset renamed

--- dataset/resie.py
+++ dataset/resie.py
@@ -1,9 +1,18 @@
 import cv2
 import os
 from os import walk
+i = 0
 for files in os.listdir(os.getcwd()):
 	print(files)
 	if(".jpg" in files):
+#compress of image
+		#image = cv2.imread(files)
+		#image = cv2.resize(image,None,fx=0.125, fy=0.125, interpolation=cv2.INTER_AREA)
+		#cv2.imwrite(files,image)
+#rename of files
+		i = i + 1
 		image = cv2.imread(files)
-		image = cv2.resize(image,None,fx=0.125, fy=0.125, interpolation=cv2.INTER_AREA)
+		files = str(i) + ".jpg"
+		print(files)	
 		cv2.imwrite(files,image)
+
