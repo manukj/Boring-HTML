795dbc86c5c92d43cca479b08a52181461133111
manukj <manu1998kj@gmail.com>
2019-10-29 10:02:06 +0530

    fixed login and signup

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import pyrebase
 from firebase.firebase import FirebaseApplication
-
-
 import json
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template,session, escape, request, redirect, url_for
 
 from subprocess import call
+from os import urandom
 
 
 app = Flask(__name__,template_folder='template')
+app.debug = True
+app.secret_key = urandom(24)
 
 login_status = 0
 
@@ -27,29 +28,49 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 
 auth = firebase.auth()
 
-
+data1 = {}
 
 @app.route("/")
 def renderIndexPage():
-   return render_template("index.html")
+    if 'username' in session:
+        return render_template('dashboard.html',result = data1)
+    return render_template("index.html")
 
 @app.route("/renderLoginAccessPage", methods = ['GET', 'POST'])
 def renderLoginAccessPage():
-    if request.method == 'GET':
+    if request.method == 'POST':
         firebase = pyrebase.initialize_app(firebaseConfig)
         auth = firebase.auth()
         data = request.form.to_dict()
         print("JSON data: ", data)
+        print(str(data.get("email")))
+        print(str(data.get("password")))
         email = str(data.get("email"))
         password = str(data.get("password"))
         try:
             user = auth.sign_in_with_email_and_password(email,password)
             print("added")
-            render_template('dashboard.html',result = data)
+            db = firebase.database()
+            all_users = db.child("/").get()
+            for user in all_users.each():
+                users = user.key()
+                values = user.val()
+                print (values.get("email").encode('utf-8'))
+                if email == values.get("email").encode('utf-8'):
+                    print("found")
+                    break
+
+            data1["firstName"] = values.get("firstName").encode('utf-8')
+            data1["lastName"] = values.get("lastName").encode('utf-8')
+            data1["password"] = "password"
+            data1["email"] = values.get("email").encode('utf-8')
+            data1["project"] =  values.get("project")
+            session['username'] = email
+            return redirect(url_for('renderIndexPage'))
         except Exception as e:
-            print(e)
-            e = json.loads(e.args[1])
-            e = (e["error"]["message"])
+            print("error ",e)
+            # e = json.loads(e.args[1])
+            # e = (e["error"]["message"])
             return render_template("login.html",us = e)
     return render_template("login.html")
 
@@ -66,15 +87,21 @@ def registerUser():
         password = str(data.get("password"))
         # password = password.encode('ASCII')
         # hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-        data = {}
-        data["firstName"] = firstName
-        data["lastName"] = lastName
-        data["password"] = password
-        data["email"] = email
-        data["project"] = {
-            "total" : 0,
-            "projects":{}
-        }
+
+        data1["firstName"] = firstName
+        data1["lastName"] = lastName
+        data1["password"] = password
+        data1["email"] = email
+	
+	pjt = {}
+	projects = {}
+	pjt["Name"] = "Project1"
+	pjt["disc"] = "...................."
+	pjt["image"] = "image path"	
+	
+	projects['total'] = 0
+	projects['project'] = pjt
+        data1["project"] = projects
         # firebase = FirebaseApplication('https://boring-html.firebaseio.com/', None)
         firebase = pyrebase.initialize_app(firebaseConfig)
 
@@ -88,16 +115,20 @@ def registerUser():
         db = firebase.database()
         db.child(firstName).set(data)
         print("added")
-        return render_template('dashboard.html',result = data)
+        session['username'] = email
+        return redirect(url_for('renderIndexPage'))
     else:
         return render_template("register.html")
 
 
-# logout
-@app.route("/login")
-def dashboard():
-    login_status = 0
-    return render_template('index.html')
+
+
+
+@app.route("/logOut")
+def logOut():
+    session.pop('username')
+    return redirect(url_for('renderIndexPage'))
+
 
 if __name__ == '__main__':
    flag_login = 0
