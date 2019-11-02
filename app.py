4fea89cfdead93a9ce697234155793272a1f172f
manukj <manu1998kj@gmail.com>
2019-11-03 00:17:53 +0530

    before getting the dataset

--- app.py
+++ app.py
@@ -35,16 +35,18 @@ def renderIndexPage():
 
 @app.route("/renderLoginAccessPage", methods = ['GET', 'POST'])
 def renderLoginAccessPage():
-    if request.method == 'GET':
+    if request.method == 'POST':
         firebase = pyrebase.initialize_app(firebaseConfig)
         auth = firebase.auth()
         data = request.form.to_dict()
         print("JSON data: ", data)
-        email = str(data.get("email"))
+        firstName = str(data.get("firstName"))
         password = str(data.get("password"))
         try:
             user = auth.sign_in_with_email_and_password(email,password)
-            print("added")
+            db = firebase.database()
+            data = db.child(firstName).get()
+            print(data)
             render_template('dashboard.html',result = data)
         except Exception as e:
             print(e)
@@ -73,31 +75,31 @@ def registerUser():
         data["email"] = email
         data["project"] = {
             "total" : 0,
-            "projects":{}
+            "projects": 0 
         }
         # firebase = FirebaseApplication('https://boring-html.firebaseio.com/', None)
         firebase = pyrebase.initialize_app(firebaseConfig)
-
         auth = firebase.auth()
         try:
             auth.create_user_with_email_and_password(email,password)
+            db = firebase.database()
+            db.child(firstName).set(data)
+            print("added")
+            return render_template('dashboard.html',result = data)
         except Exception as e:
             e = json.loads(e.args[1])
             e = (e["error"]["message"])
             return render_template("register.html",us = e)
-        db = firebase.database()
-        db.child(firstName).set(data)
-        print("added")
-        return render_template('dashboard.html',result = data)
+
     else:
         return render_template("register.html")
 
 
 # logout
-@app.route("/login")
+@app.route("/edit.html")
 def dashboard():
     login_status = 0
-    return render_template('index.html')
+    return render_template('edit.html')
 
 if __name__ == '__main__':
    flag_login = 0
